c175526af02d0375f526e604b23e27442a4f8ea8
Your Nameranjini <ranjini782000@gmail.com>
2023-03-09 18:33:53 +0530

    first commit

---
+++ app1/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def dhoni(request):
+    return HttpResponse('MSD is best finisher')
---
+++ app2/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def virat(request):
+    return HttpResponse('virat is best batsaman in the world')
